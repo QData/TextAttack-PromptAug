f67a15e9e57b69c643acfa96d62703798ca27bcf
k-ivey <kivey1@gmail.com>
2023-03-24 12:36:21 -0400

    Refactor main for arguments and data loading
    Refactor the main function to
    * Accept command line arguments
    * Use the data loader class to obtain data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import random
 import json
@@ -5,64 +6,45 @@ import os
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import T5Tokenizer, T5ForConditionalGeneration
-from question_generator import QuestionGenerator
+
+from datasets.loader import ShapeDataset
 
 
 if __name__ == "__main__":
     CACHE_DIR = "/p/qdatatext/ki4km/models/"
-    DATA_DIR = "datasets/data/canvases"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", choices=["flan"], required=True)
+    parser.add_argument("-q", "--question", choices=["existence", "transitive"], required=True)
+    parser.add_argument("-s", "--num-shapes", default=1, type=int)
+    parser.add_argument("-n", "--num-examples", default=0, type=int)
+    args = parser.parse_args()
 
     is_gpu = torch.cuda.is_available()
-
     if is_gpu:
-        print("Using GPU")
+        print("Using GPU...")
     else:
-        print("Using CPU")
+        print("Using CPU...")
 
-    # tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=CACHE_DIR)
-    # model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", device_map="auto", cache_dir=CACHE_DIR)
-    # subfolder = "GPT-J"
+    if args.model == "flan":
+        tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl", cache_dir=CACHE_DIR)
+        model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", device_map="auto", cache_dir=CACHE_DIR)
+        subfolder = "T5"
+    elif args.model == "gpt-j":
+        tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=CACHE_DIR)
+        model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", device_map="auto", cache_dir=CACHE_DIR)
+        subfolder = "GPT-J"
 
-    # tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl", cache_dir=CACHE_DIR)
-    # model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", device_map="auto", cache_dir=CACHE_DIR)
-    subfolder = "T5"
 
-    results_folder = f"results/{subfolder}/transitivity"
+    results_folder = f"results/{subfolder}/{args.question}"
     if not os.path.isdir(results_folder):
         os.makedirs(results_folder)
 
-    positive = 0
-    negative = 0
-    n_examples = 100
-    for i in range(0, n_examples):
-        filepath = f"{DATA_DIR}/canvas_3_{i}.json"
-        outpath = f"{results_folder}/canvas_3_{i}.json"
-
-        gen = QuestionGenerator.from_file(filepath)
-        #questions = gen.generate_existence_questions()
-
-        # Have an even split
-        # is_answer_yes = i < (n_examples // 2) 
-        # if is_answer_yes:
-        #     question = random.choice([question for question, answer in questions if answer == "Yes"])
-        #     expected = "Yes"
-        # else:
-        #     question = random.choice([question for question, answer in questions if answer == "No"])
-        #     expected = "No"
-
-        # input_string = gen.generate_full_description() + question + " Answer with 'Yes' or 'No'.\nAnswer:"
-        # question = "Is there a yellow shape in the canvas?"
-        #input_string = gen.generate_full_description() + "\nIs there a yellow triangle in the canvas?" + "\nAnswer: No\n" + question + "\nAnswer:"
-        #input_string = gen.generate_full_description() + f"\nQuestion: {question}" + "\nAnswer:"
-
-        # input_string = gen.generate_short_description() + f"\nQuestion: {question}" + "\nAnswer:"
-        #input_string = gen.generate_short_description() + question + "\nAnswer:"
-
-        input_string, expected = gen.generate_transitivity_promopt(horizontal=True)
-        question = input_string.split("Question")[1]
-
-        print(input_string)
-        exit()
+    data = ShapeDataset(args.num_shapes)
+    for i in range(0, args.num_examples):
+        outpath = f"{results_folder}/canvas_{args.num_shapes}_{i}.json"
+        description, question, expected = data[i]
+        input_string = description + "\nQuestion: " + question
 
         input_ids = tokenizer(input_string, return_tensors="pt").input_ids
         if is_gpu:
@@ -77,21 +59,12 @@ if __name__ == "__main__":
         )
         answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-        # if is_answer_yes and "yes" in answer.lower():
-        #     positive += 1
-        # elif not is_answer_yes and "no" in answer.lower():
-        #     negative += 1
-
         with open(outpath, "w") as f:
             result = {
-                "context": input_string, 
+                "description": description,
                 "question": question, 
                 "expected": expected,
                 "actual": answer
             }
             json.dump(result, f, indent=4)  
 
-    # Print results
-    # print(f"Accuracy: {(positive + negative) / n_examples}")
-    # print(f"Positive Accuracy: {positive / (n_examples // 2)}")
-    # print(f"Negative Accuracy: {negative / (n_examples // 2)}")
