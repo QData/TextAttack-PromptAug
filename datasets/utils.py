10f5a137ed0d9c91514f16af7cbcb14438b33aed
k-ivey <kivey1@gmail.com>
2023-04-17 19:39:00 -0400

    Fix edge case of object enclosing another

--- datasets/utils.py
+++ datasets/utils.py
@@ -1,6 +1,6 @@
 import json
 
-from shapes import Shape
+from datasets.shapes import Shape
 
 def json_to_shapes(filepath):
     with open(filepath, "r") as file:
@@ -26,14 +26,18 @@ def get_relationships(shapes):
         
         for j, shape2 in enumerate(shapes):
             if i != j:
-                if shape2.left < shape1.left:
-                    relations["Left"].append(j)
-                if shape2.right > shape1.right:
-                    relations["Right"].append(j)
-                if shape2.top > shape1.top:
-                    relations["Above"].append(j)
-                if shape2.bottom < shape1.bottom:
-                    relations["Below"].append(j)
+                # Pay attention to the case where a larger object "swallows" a smaller one
+                if not (shape2.left < shape1.left and shape2.right > shape1.right):
+                    if shape2.left < shape1.left:
+                        relations["Left"].append(j)
+                    if shape2.right > shape1.right:
+                        relations["Right"].append(j)
+
+                if not (shape2.top > shape1.top and shape2.bottom < shape1.bottom):
+                    if shape2.top > shape1.top:
+                        relations["Above"].append(j)
+                    if shape2.bottom < shape1.bottom:
+                        relations["Below"].append(j)
 
         # Sort all the relationships
         relations["Left"].sort(key=lambda i: shapes[i].left, reverse=True)
